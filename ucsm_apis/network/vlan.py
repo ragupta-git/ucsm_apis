fad88e36ee5fb089a5a027330facaa2babcac1fa
Rahul Gupta <ragupta4@cisco.com>
2017-04-06 10:31:19 +0530

    commiting vlan
    Signed-off-by: Rahul Gupta <ragupta4@cisco.com>

--- ucsm_apis/network/vlan.py
+++ ucsm_apis/network/vlan.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This module performs the Vlan related operation
+This module performs the vlan related operation
 """
 from ucsmsdk.ucsexception import UcsOperationError
 
